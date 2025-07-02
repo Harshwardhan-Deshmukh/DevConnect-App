338b331ab9450f54760c863b7ca01480be805630
Harshwardhan Deshmukh <harshwardhanet7007@gmail.com>
2025-07-02 14:11:45 +0530

    exception handling logic added

--- src/main/java/com/devconnect/devconnect_app/advices/GlobalGraphQlExceptionHandler.java
+++ src/main/java/com/devconnect/devconnect_app/advices/GlobalGraphQlExceptionHandler.java
@@ -2,8 +2,12 @@ package com.devconnect.devconnect_app.advices;
 
 import com.devconnect.devconnect_app.exception.ResourceNotFoundException;
 import graphql.GraphQLError;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalGraphQlExceptionHandler {
@@ -17,6 +21,27 @@ public class GlobalGraphQlExceptionHandler {
 
     @GraphQlExceptionHandler(RuntimeException.class)
     public GraphQLError handleRuntimeException(RuntimeException exception) {
+        return GraphQLError.newError()
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @GraphQlExceptionHandler(MethodArgumentNotValidException.class)
+    public GraphQLError handleValidationException(MethodArgumentNotValidException exception) {
+        List<String> errors = exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        String errorResult = String.join(", ", errors);
+
+        return GraphQLError.newError()
+                .message(errorResult)
+                .build();
+    }
+
+    @GraphQlExceptionHandler(Exception.class)
+    public GraphQLError handleInternalServerError(Exception exception) {
         return GraphQLError.newError()
                 .message(exception.getMessage())
                 .build();
